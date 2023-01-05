b6a43cbfbc1458afc240a77f1a4a2c68c120f722
Mosope <moshopeowo@gmail.com>
2023-01-05 22:16:57 +0100

    completed

--- app.js
+++ app.js
@@ -10,12 +10,14 @@ const bcrypt = require("bcrypt");
 const passport = require("passport")
 const flash = require("express-flash")
 const session = require("express-session")
+const methodOverride = require("method-override");
 
 const initializePassport = require("./passport-config")
 
 initializePassport(
     passport, 
-    email => {return users.find(user => user.email ===  email)}
+    email => {return users.find(user => user.email ===  email)},
+    id => {return users.find(user => user.id ===  id)}
     )
 const users = [];
 
@@ -31,20 +33,21 @@ app.use(session({
 }));
 app.use(passport.initialize())
 app.use(passport.session())
+app.use(methodOverride("_method"))
 
 //home route
-app.get("/", (req, res) => {
-    res.render("index", {title: "Login System", name: "Mosope"})
+app.get("/", checkAuthenticated, (req, res) => {
+    res.render("index", {title: "Login System", name: req.user.name})
 });
 
 //login route
-app.get("/login", (req, res) => {
+app.get("/login", checkNotAuthenticated, (req, res) => {
     res.render("login", {title: "Login"})
 
 });
 
 //post to login route
-app.post("/login",  passport.authenticate("local", {
+app.post("/login",  checkNotAuthenticated, passport.authenticate("local", {
     successRedirect: "/",
     failureRedirect: "/login",
     failureFlash: true
@@ -53,12 +56,12 @@ app.post("/login",  passport.authenticate("local", {
 
 
 //registration route
-app.get("/register", (req, res) => {
+app.get("/register", checkNotAuthenticated, (req, res) => {
     res.render("register", {title: "Registration"})
 })
 
 //post to registration route
-app.post("/register", async (req, res) => {
+app.post("/register", checkNotAuthenticated, async (req, res) => {
     try{
         const hashedPassword = await bcrypt.hash(req.body.password, 10);
         users.push({
@@ -74,5 +77,28 @@ app.post("/register", async (req, res) => {
     console.log(users)
 
 });
+
+app.delete("/logout", (req, res) => {
+    req.logOut((err) => {
+        if (err) { 
+            return next(err); 
+        }});
+    res.redirect("/login");
+})
+
+
+function checkAuthenticated(req, res, next) {
+    if(req.isAuthenticated()){
+        return next();
+    }
+    res.redirect("/login")
+}
+
+function checkNotAuthenticated(req, res, next) {
+    if(req.isAuthenticated()){
+        return res.redirect("/");
+    }
+    next()
+}
 
 app.listen("3000", () => {console.log(`Listening on port ${PORT}...`)});
